aa7e6a26a83460169213f9db11b7a7ec1088e164
kcoul <kieran.coulter@gmail.com>
2024-03-01 13:58:01 -0800

    Stubs for Application build

---
+++ app/MainComponent.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+namespace AudioApp
+{
+class MainComponent : public juce::AudioAppComponent
+{
+public:
+    MainComponent();
+
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
+    void releaseResources() override;
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
+
+    void paint(juce::Graphics&) override;
+    void resized() override;
+
+private:
+    juce::AudioDeviceSelectorComponent selector {
+        deviceManager, 2, 2, 2, 2, false, false, true, false};
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
+};
+
+} // namespace AudioApp
